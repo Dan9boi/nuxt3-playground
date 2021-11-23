643c21663558e10e7e2761253476e934513b1eed
Danni <dla@novicell.dk>
2021-11-23 14:59:00 +0100

    second commit

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+
+const data = reactive({ name: 'hestedyr' })
+
+
+const changeName = () => {
+  $fetch('https://randomuser.me/api/')
+    .then(res => {
+      console.log(res)
+      data.name = res.results[0].name.first;
+    })
+}
+</script>
+
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div>
+      <h1>Hello {{ data.name }}</h1>
+      <button @click="changeName">Change Name</button>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,57 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
-})
+    publicRuntimeConfig: {
+        BASE_URL: process.env.BASE_URL || 'https://127.0.0:3000',
+        API_URL: process.env.API_URL || 'https://dev-novicell-geekchallenge.s1.umbraco.io/'
+    },
+    css: [
+        'normalize.css/normalize.css',
+        '@/assets/css/master.css'
+    ],
+    build: {
+        postcss: {
+            postcssOptions: {
+                config: {
+                    stage: 0
+                },
+                plugins: {
+                    'postcss-import': {},
+                    'postcss-nested': {},
+                    'postcss-preset-env': {
+                        stage: 0, // default stage is 2
+                        preserve: false,
+                        importFrom: [
+                            'assets/css/_variables.css'
+                        ],
+                        autoprefixer: {
+                            grid: true
+                        },
+                        features: {
+                            'color-mod-function': { unresolved: 'warn' },
+                            'custom-media-queries': {},
+                        },
+                        browsers: ['>= 5% in DK', 'not ie 11']
+                    },
+                    'cssnano': {
+                        preset: ["default", {
+                            autoprefixer: false, // preset-env does this
+                            discardComments: {
+                                removeAll: true
+                            },
+                            mergeLonghand: true,
+                            colormin: false,
+                            zindex: false,
+                            discardUnused: {
+                                fontFace: false
+                            }
+                        }],
+                    },
+                    'postcss-reporter': {
+                        clearReportedMessages: true
+                    },
+                }
+            }
+        }
+    },
+});
