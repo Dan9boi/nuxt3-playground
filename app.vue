36fc8fe34ed2ff1a1acc9fccbc8a633c02113d68
Danni <dla@novicell.dk>
2021-12-13 13:50:33 +0100

    added components and nav

--- app.vue
+++ app.vue
@@ -1,27 +1,36 @@
 <script setup>
-// import { reactive } from "vue";
-
-// const data = reactive({ name: 'hestedyr' })
-
-
-// const changeName = () => {
-//   $fetch('https://randomuser.me/api/')
-//     .then(res => {
-//       console.log(res)
-//       data.name = res.results[0].name.first;
-//     })
-// }
-
-const { data } = await useFetch(`https://randomuser.me/api/`);
-
+const { data } = await useFetch(`https://cdn.umbraco.io/content/url`, {
+  headers: {
+    "Accept-Language": "en-US",
+    "umb-project-alias": "dev-novicell-geekchallenge",
+  },
+  params: {
+    url: "/home/",
+  },
+});
+const formatNavigationLink = (link) => link?.toLowerCase().replaceAll(" ", "-");
 </script>
-
 
 <template>
   <div>
-    <div>
-      <h1>Hello {{ data.results[0].name.first }}</h1>
-      <button>Change Name</button>
+    <!-- <Head lang="en-US">
+      <Title>{{ data.title }} title</Title>
+      <Meta name="description" :content="data.description" />
+      <Meta name="og:title" :content="data.socialTitle" />
+      <Meta name="og:description" :content="data.socialDescription" />
+      <Meta name="og:type" content="website" />
+      <Meta name="og:image" :content="$config.apiUrl + data.socialImage.src" />
+    </Head>-->
+
+    <the-header :items="data.pageContent"></the-header>
+
+    <div v-for="section in data.pageContent" :key="section._id">
+      {{section.contentTypeAlias}}
+      <!-- <component
+        :is="section.contentTypeAlias"
+        :content="section"
+        :navLink="formatNavigationLink(section.navigationTag)"
+      ></component> -->
     </div>
   </div>
 </template>
