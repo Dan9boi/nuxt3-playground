8c2992bcdfd55a981d23341a78b84cf220f8e737
Danni <dla@novicell.dk>
2021-12-13 13:34:55 +0100

    try $fetch

--- app.vue
+++ app.vue
@@ -1,24 +1,27 @@
 <script setup>
-import { reactive } from "vue";
+// import { reactive } from "vue";
 
-const data = reactive({ name: 'hestedyr' })
+// const data = reactive({ name: 'hestedyr' })
 
 
-const changeName = () => {
-  $fetch('https://randomuser.me/api/')
-    .then(res => {
-      console.log(res)
-      data.name = res.results[0].name.first;
-    })
-}
+// const changeName = () => {
+//   $fetch('https://randomuser.me/api/')
+//     .then(res => {
+//       console.log(res)
+//       data.name = res.results[0].name.first;
+//     })
+// }
+
+const { data } = await useFetch(`https://randomuser.me/api/`);
+
 </script>
 
 
 <template>
   <div>
     <div>
-      <h1>Hello {{ data.name }}</h1>
-      <button @click="changeName">Change Name</button>
+      <h1>Hello {{ data.results[0].name.first }}</h1>
+      <button>Change Name</button>
     </div>
   </div>
 </template>
